5c19ba0264b0ba88474f96f2456444da38158a89
Ezequiel <ezzemontoya@outlook.com>
2017-06-30 18:15:25 -0300

    ajuste del main class

--- src/main/java/megafono/mainclass/mainClass.java
+++ src/main/java/megafono/mainclass/mainClass.java
@@ -5,37 +5,6 @@ import megafono.services.UsuarioServices;
 
 public class mainClass {
 
-	// public static void main(String[] args) {
-	// Tag t = new Tag("Futbol");
-	// Tag b = new Tag("Tenis");
-	// Tag d = new Tag("Deporte");
-	// d.agregarTagHijo(t);
-	// d.agregarTagHijo(b);
-	// TagServices ts = TagServices.getTagService();
-	// ts.guardar(d);
-	// }
-
-	// public static void main(String[] args) {
-	// TagServices ts = TagServices.getTagService();
-	// ts.borrar(new Tag ("Futbol"));
-	// ArrayList<Tag> tags = ts.obtenerTags();
-	// for (Tag t : tags){
-	// System.out.println(t.toString());
-	// }
-	// }
-
-	// public static void main(String[] args) {
-	// Cliente cliente = new Cliente("prueba", "prueba1", "123456",
-	// "ezzemontoya@gmail.com");
-	// ClienteServices cs = ClienteServices.getClienteServices();
-	// cs.guardar(cliente);
-	// }
-
-//	public static void main(String[] args) {
-//		CamapaniaServices cs = CamapaniaServices.getCampañaServices();
-//		cs.ejecutar();
-//	}
-
 	public static void main(String[] args) {
 		Usuario user = new Usuario("admin", "admin");
 		UsuarioServices us = UsuarioServices.getusuarioServices();
