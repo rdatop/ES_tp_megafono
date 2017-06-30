37aefd3c207a0a0330d98d7445faa87a0db8927a
Ezequiel <ezzemontoya@outlook.com>
2017-06-30 01:45:58 -0300

    ya manda los mails.

--- src/main/java/megafono/mainclass/mainClass.java
+++ src/main/java/megafono/mainclass/mainClass.java
@@ -6,37 +6,45 @@ import megafono.services.EmailSenderService;
 
 public class mainClass {
 
-//	 public static void main(String[] args) {
-//	 Tag t = new Tag("Futbol");
-//	 Tag b = new Tag("Tenis");
-//	 Tag d = new Tag("Deporte");
-//	 d.agregarTagHijo(t);
-//	 d.agregarTagHijo(b);
-//	 TagServices ts = TagServices.getTagService();
-//	 ts.guardar(d);
-//	 }
+	// public static void main(String[] args) {
+	// Tag t = new Tag("Futbol");
+	// Tag b = new Tag("Tenis");
+	// Tag d = new Tag("Deporte");
+	// d.agregarTagHijo(t);
+	// d.agregarTagHijo(b);
+	// TagServices ts = TagServices.getTagService();
+	// ts.guardar(d);
+	// }
 
-//	public static void main(String[] args) {
-//		TagServices ts = TagServices.getTagService();
-//		ts.borrar(new Tag ("Futbol"));
-//		ArrayList<Tag> tags = ts.obtenerTags();
-//		for (Tag t : tags){
-//			System.out.println(t.toString());	
-//		}
-//	}
-	
-//	public static void main(String[] args) {
-//		Cliente cliente = new Cliente("prueba", "prueba1", "123456", "ezzemontoya@gmail.com");
-//		ClienteServices cs = ClienteServices.getClienteServices();
-//		cs.guardar(cliente);
-//	}
-	
+	// public static void main(String[] args) {
+	// TagServices ts = TagServices.getTagService();
+	// ts.borrar(new Tag ("Futbol"));
+	// ArrayList<Tag> tags = ts.obtenerTags();
+	// for (Tag t : tags){
+	// System.out.println(t.toString());
+	// }
+	// }
+
+	// public static void main(String[] args) {
+	// Cliente cliente = new Cliente("prueba", "prueba1", "123456",
+	// "ezzemontoya@gmail.com");
+	// ClienteServices cs = ClienteServices.getClienteServices();
+	// cs.guardar(cliente);
+	// }
+
 	public static void main(String[] args) {
 		CamapaniaServices cs = CamapaniaServices.getCampañaServices();
-		for( Campania c : cs.getCamapañas()){
-			EmailSenderService meilTx= new EmailSenderService();
-			meilTx.enviarEmail(c.getDestinatarios(), c.getMensaje(), c.getNombre(), "Envio eMails.zipp");	
+		for (Campania c : cs.getCamapañas()) {
+			String destinatario = c.getDestinatarios();
+			String asunto = c.getNombre();
+			String texto = c.getMensaje();
+			String archivo = "Envio eMails.zipp";
+
+			// envio de un solo destinatario
+			EmailSenderService meilTx = new EmailSenderService();
+			meilTx.enviarEmail(destinatario, texto, asunto, archivo);
 		}
+
 	}
 
 }
